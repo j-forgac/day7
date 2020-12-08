fceb2f568f4044203a895b93e2412051f066ba0f
Jan Forgac <jan.forgac@educanet.cz>
2020-12-08 18:44:54 +0100

    gold

--- day_7/src/Executor.java
+++ day_7/src/Executor.java
@@ -3,71 +3,35 @@ import java.util.ArrayList;
 public class Executor {
 	public void run() {
 		String[] rawInput = InputLoader.getInput();
-		String[][] processedInput = new String[rawInput.length][5];
-		int shorten = 0;
-		ArrayList<String> bagsContainingGoldenBag = new ArrayList<>();
-		bagsContainingGoldenBag.add("shiny gold bag");
+		String[] processedInput = new String[rawInput.length];
+		int[] processedInputValues = new int[rawInput.length];
+		ArrayList<Integer> visitedLines = new ArrayList<>();
+		int lineIndex = 0;
+		int accumulator = 0;
 		for (int x = 0; x < rawInput.length; x++) {
-			rawInput[x] = rawInput[x].replaceAll("contain", "#");
-			rawInput[x] = rawInput[x].replaceAll(",", "#");
-
-			processedInput[x] = rawInput[x].split("#");
-			for (int y = 0; y < processedInput[x].length; y++) {
-				if (y == 0) {
-					processedInput[x][y] = processedInput[x][y].substring(0, processedInput[x][y].length() - 2);
-				} else {
-					if (processedInput[x][y].charAt(1) == '1') {
-						shorten = 0;
-					} else {
-						shorten = 1;
-					}
-					if (y == processedInput[x].length - 1) {
-						processedInput[x][y] = processedInput[x][y].substring(3, processedInput[x][y].length() - 1 - shorten);
-					} else {
-						processedInput[x][y] = processedInput[x][y].substring(3, processedInput[x][y].length() - shorten);
-					}
-				}
+			processedInput[x] = rawInput[x].substring(0, 3);
+			processedInputValues[x] = Integer.parseInt(rawInput[x].substring(4));
+		}
+		while (!contains(lineIndex, visitedLines)) {
+			if (processedInput[lineIndex].equals("jmp")) {
+				lineIndex += +processedInputValues[lineIndex];
+			} else if (processedInput[lineIndex].equals("acc")) {
+				accumulator += processedInputValues[lineIndex];
+				lineIndex++;
+			} else {
+				lineIndex++;
 			}
 		}
-		startAgain:
-		for(int x = 0; x < processedInput.length; x++){
-			for(int y = 1; y < processedInput[x].length; y++){
-				for(int bags = 0; bags < bagsContainingGoldenBag.size(); bags++){
-					if(processedInput[x][y].equals(bagsContainingGoldenBag.get(bags))){
-						if(!bagsContainingGoldenBag.contains(processedInput[x][0])){
-							x = -1;
-							continue startAgain;
-						}
-					}
-				}
+		System.out.println(accumulator);
+	}
+
+	public boolean contains(int num, ArrayList<Integer> arr) {
+		for (int x = 0; x < arr.size(); x++) {
+			if (arr.get(x) == num) {
+				return true;
 			}
 		}
-		System.out.println(bagsContainingGoldenBag.size() - 1);
-
+		arr.add(num);
+		return false;
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-				for(int bags = 0; bags < bagsContainingGoldenBag.size(); bags++){
-					if(processedInput[x][y].equals(bagsContainingGoldenBag.get(bags))){
-						for(int checkBags = 0; checkBags < bagsContainingGoldenBag.size(); checkBags++){
-							if(processedInput[x][y].equals(bagsContainingGoldenBag.get(checkBags))){
-								alreadyOnList = true;
-							} else {
-								alreadyOnList = false;
-							}
-						}
-					}
-				}*/
--- day_7/src/Executor2.java
+++ day_7/src/Executor2.java
@@ -1,4 +1,66 @@
+import java.util.ArrayList;
+
 public class Executor2 {
 	public void run() {
+		String[] rawInput = InputLoader.getInput();
+		String[] processedInput = new String[rawInput.length];
+		int[] processedInputValues = new int[rawInput.length];
+		ArrayList<Integer> visitedLines = new ArrayList<>();
+		int lineIndex = 0;
+		int accumulator = 0;
+		for (int x = 0; x < rawInput.length; x++) {
+			processedInput[x] = rawInput[x].substring(0, 3);
+			processedInputValues[x] = Integer.parseInt(rawInput[x].substring(4));
+		}
+
+		endThisShit:
+		for (int changedLine = 0;changedLine < processedInput.length; changedLine++) {
+			removeElements(visitedLines);
+			lineIndex = 0;
+			accumulator = 0;
+			while(!contains(lineIndex, visitedLines)){
+				if(lineIndex == changedLine){
+					if (processedInput[lineIndex].equals("jmp")) {
+						lineIndex++;
+					} else if (processedInput[lineIndex].equals("acc")) {
+						accumulator += processedInputValues[lineIndex];
+						lineIndex++;
+
+					} else {
+						lineIndex += +processedInputValues[lineIndex];
+					}
+				} else {
+					if (processedInput[lineIndex].equals("jmp")) {
+						lineIndex += +processedInputValues[lineIndex];
+					} else if (processedInput[lineIndex].equals("acc")) {
+						accumulator += processedInputValues[lineIndex];
+						lineIndex++;
+					} else {
+						lineIndex++;
+					}
+				}
+				if(lineIndex == processedInput.length){
+					System.out.println(accumulator);
+					break endThisShit;
+				}
+			}
+		}
+
+	}
+
+	public boolean contains(int num, ArrayList<Integer> arr) {
+		for (int x = 0; x < arr.size(); x++) {
+			if (arr.get(x) == num) {
+				return true;
+			}
+		}
+		arr.add(num);
+		return false;
+	}
+
+	public void removeElements(ArrayList<Integer> arr){
+		for (int x = arr.size() - 1; x >= 0; x--) {
+			arr.remove(x);
+		}
 	}
 }
